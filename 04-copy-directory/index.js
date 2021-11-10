f0daab47e5b3774cd026de825e3df85c7047325d
MakFil <makfil1177@gmail.com>
2021-11-10 19:54:46 +0200

    feat: add task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const path = require('path');
+const { mkdir, copyFile, readdir } = require('fs/promises');
 
+const filesPath = path.join(__dirname, 'files');
+const filesCopyPath = path.join(__dirname, 'files-copy');
+
+function copyFolder() {
+  mkdir(filesCopyPath, { recursive: true })
+    .then(() => readdir(filesPath, { withFileTypes: true }))
+    .then((res) =>
+      res.forEach((item) => {
+        copyFile(path.join(filesPath, item.name), path.join(filesCopyPath, item.name));
+      })
+    );
+}
+
+copyFolder();
