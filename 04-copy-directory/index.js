b1f47d5b4afc4840d34b41fca96650bd999f0950
MakFil <makfil1177@gmail.com>
2021-11-12 16:52:03 +0200

    fix: task 4 unlink

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,11 +1,17 @@
 const path = require('path');
-const { mkdir, copyFile, readdir } = require('fs/promises');
+const { mkdir, copyFile, readdir, unlink } = require('fs/promises');
 
 const filesPath = path.join(__dirname, 'files');
 const filesCopyPath = path.join(__dirname, 'files-copy');
 
 function copyDir() {
   mkdir(filesCopyPath, { recursive: true })
+    .then(() => readdir(filesCopyPath, { withFileTypes: true }))
+    .then(async (res) => {
+      for await (let file of res) {
+        await unlink(path.join(filesCopyPath, file.name));
+      }
+    })
     .then(() => readdir(filesPath, { withFileTypes: true }))
     .then((res) =>
       res.forEach((item) => {
